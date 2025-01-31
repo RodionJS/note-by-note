48babe44f3605f6148d02b5d77649affb5c2c353
Rodion Kuznetsov <kuznetsovrodion00@gmail.com>
2025-02-01 00:27:00 +0100

    Big screen fixes

--- src/Components/CreateWidget.jsx
+++ src/Components/CreateWidget.jsx
@@ -29,7 +29,7 @@ const CreateWidget = ({ notes, setNotes, setIsModalOpen }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white px-8 py-12 sm:py-4 rounded shadow-lg min-h-5/6 max-h-[90vh] w-screen sm:h-5/6 sm:w-5/6 md:w-4/5 xl:w-3/4 2xl:w-2/3 relative">
+      <div className="bg-white px-8 py-12 sm:py-4 rounded shadow-lg h-screen w-screen sm:h-5/6 sm:w-5/6 md:w-4/5 xl:w-3/4 2xl:w-2/3 flex flex-col">
         <button
           className="rotate-45 absolute top-7 sm:top-0 right-1 text-4xl font-light duration-300 sm:opacity-50 hover:opacity-100"
           onClick={() => setIsModalOpen(false)}
@@ -38,7 +38,7 @@ const CreateWidget = ({ notes, setNotes, setIsModalOpen }) => {
         </button>
         <h3 className="text-xl font-semibold mb-4">New note</h3>
         <p className="mb-4">Create your note!</p>
-        <form>
+        <form className="flex flex-1 flex-col">
           <input
             value={input.theme}
             maxLength={18}
@@ -50,23 +50,25 @@ const CreateWidget = ({ notes, setNotes, setIsModalOpen }) => {
           <textarea
             value={input.content}
             maxLength={1000}
-            className="w-full h-80 sm:h-72 resize-none py-2 px-4 mb-4 border-2 rounded transition-colors duration-200 focus:border-gray-500 focus:outline-none"
+            className="w-full flex-1 resize-none py-2 px-4 mb-4 border-2 rounded transition-colors duration-200 focus:border-gray-500 focus:outline-none"
             placeholder="Type your note here..."
             onChange={(e) => setInput({ ...input, content: e.target.value })}
           ></textarea>
         </form>
-        <button
-          className="m-2 px-4 py-2 relative -bottom-10 sm:bottom-0 text-white rounded duration-300 bg-blue-500 hover:bg-blue-600 active:scale-95"
-          onClick={handelCreateNote}
-        >
-          Create
-        </button>
-        <button
-          className="m-2 px-4 py-2 relative -bottom-10 sm:bottom-0 text-white rounded duration-300 bg-red-500 hover:bg-red-600 active:scale-90"
-          onClick={() => setIsModalOpen(false)}
-        >
-          Close
-        </button>
+        <div className="flex justify-center gap-2 mb-3">
+          <button
+            className="px-4 py-2 text-white rounded duration-300 bg-blue-500 hover:bg-blue-600 active:scale-95"
+            onClick={handelCreateNote}
+          >
+            Create
+          </button>
+          <button
+            className="px-4 py-2 text-white rounded duration-300 bg-red-500 hover:bg-red-600 active:scale-90"
+            onClick={() => setIsModalOpen(false)}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Components/ViewWidget.jsx
+++ src/Components/ViewWidget.jsx
@@ -3,26 +3,32 @@ import React from "react";
 const ViewWidget = ({ theme, content, setViewVisible }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white px-8 py-12 sm:py-4 rounded shadow-lg h-screen w-screen sm:h-fit sm:w-5/6 md:w-4/5 xl:w-3/4 2xl:w-2/3 relative">
+      <div className="bg-white px-8 py-12 sm:py-4 rounded shadow-lg h-screen w-screen sm:h-fit sm:w-5/6 md:w-4/5 xl:w-3/4 2xl:w-2/3 flex flex-col">
         <button
           className="rotate-45 absolute top-7 sm:top-0 right-1 text-4xl font-light duration-300 sm:opacity-0 hover:opacity-100"
           onClick={() => setViewVisible(false)}
         >
           +
         </button>
-        <h1 className="w-full p-2 mb-4 text-2xl">{theme}</h1>
-        <textarea
-          value={content}
-          readOnly
-          className="w-full h-96 sm:h-72 resize-none py-2 px-4 mb-4 border-2 rounded transition-colors duration-200 focus:border-gray-500 focus:outline-none"
-        ></textarea>
 
-        <button
-          className="m-2 px-4 py-2 relative -bottom-10 sm:bottom-0 text-white rounded duration-300 bg-red-500 hover:bg-red-600 active:scale-90"
-          onClick={() => setViewVisible(false)}
-        >
-          Close
-        </button>
+        <h1 className="w-full p-2 mb-4 text-2xl">{theme}</h1>
+
+        <div className="flex-1 flex flex-col">
+          <textarea
+            value={content}
+            readOnly
+            className="w-full flex-1 min-h-56 resize-none py-2 px-4 mb-4 border-2 rounded transition-colors duration-200 focus:border-gray-500 focus:outline-none"
+          ></textarea>
+        </div>
+
+        <div className="flex justify-center mb-3">
+          <button
+            className="px-4 py-2 text-white rounded duration-300 bg-red-500 hover:bg-red-600 active:scale-90"
+            onClick={() => setViewVisible(false)}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );

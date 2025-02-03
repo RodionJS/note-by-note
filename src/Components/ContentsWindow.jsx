2268955e01009b05a7cc072d8e86ddc79962970c
Rodion Kuznetsov <kuznetsovrodion00@gmail.com>
2025-02-03 03:59:34 +0100

    Fixed plus button

--- src/Components/ContentsWindow.jsx
+++ src/Components/ContentsWindow.jsx
@@ -21,21 +21,31 @@ const ContentsWindow = ({ notes, setNotes }) => {
   return (
     <div className="flex flex-col items-center justify-start h-full text-center mt-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mx-auto w-11/12 max-w-[800px] lg:max-w-[1400px] mb-12">
-        {notes.length > 2 && (
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className={createButtonCSS}
-          >
-            Create Note
-          </button>
-        )}
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className={
+            createButtonCSS +
+            (notes.length === 1 ? " sm:hidden" : "") +
+            (notes.length < 3 ? " hidden sm:block" : "")
+          }
+        >
+          Create Note
+        </button>
         {notes.map((note) => (
           <Note key={note.id} note={note} notes={notes} setNotes={setNotes} />
         ))}
         <div className="flex flex-col min-w-72 sm:min-w-0 w-full h-52 sm:h-64 mx-auto">
-          <button onClick={() => setIsModalOpen(true)} className={plusCSS}>
-            +
-          </button>
+          <svg
+            onClick={() => setIsModalOpen(true)}
+            className={plusCSS}
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            width="68"
+            height="68"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
+          </svg>
         </div>
       </div>
       {isModalOpen && (
